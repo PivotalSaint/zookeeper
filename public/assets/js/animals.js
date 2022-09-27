ece767793f0f9e63c6bcabf8607f8838e9f65953
PivotalSaint <licensedmt@att.net>
2022-09-27 17:56:40 -0500

    changed routing

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
@@ -32,19 +30,18 @@ const getAnimals = (formData = {}) => {
   });
 
   console.log(queryUrl);
-  
-  //added in 11.3.7
+
   fetch(queryUrl)
-  .then(response => {
-    if (!response.ok) {
-      return alert('Error: ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(animalData => {
-    console.log(animalData);
-    printResults(animalData);
-  });
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
 };
 
 const handleGetAnimalsSubmit = event => {
